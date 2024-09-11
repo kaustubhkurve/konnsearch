cb1dcfe9f1ccddd16b01225ba2ed041a207cd9cc
Kaustubh Kurve <kaustubh6024@gmail.com>
2024-09-11 16:31:40 +0530

    Split main into two scripts (Kafka ingest and os index), add README

--- opensearch_index.py
+++ opensearch_index.py
@@ -1,23 +1,10 @@
 import os
 
 from src.konnsearch.connect import Connection
-from src.konnsearch.connectors.file import JSONSourceConnector
-from src.konnsearch.connectors.kafka import (
-    KafkaSinkConnector, KafkaSourceConnector
-)
+from src.konnsearch.connectors.kafka import KafkaSourceConnector
 from src.konnsearch.connectors.opensearch import OpenSearchSinkConnector
 
 if __name__ == "__main__":
-    kconfig = {
-        "bootstrap.servers": "localhost:9092"
-    }
-
-    source = JSONSourceConnector(os.path.join(os.getcwd(), "stream.jsonl"))
-    target = KafkaSinkConnector(kconfig, "cdc-events")
-
-    connection = Connection(source, target)
-    connection.sync()
-
     kconsumer_config = {
         "bootstrap.servers": "localhost:9092",
         "group.id": "kongconsumer",
